a45f8524fec8c0a1d8b33e8f32aad86faf1f038f
DELL <DELL@DESKTOP-SJU5LD2>
2023-02-17 09:35:03 +0530

    Commiting the code For Practice on 17-feb-2023 at9:30am tester sachin

---
+++ src/javaPracticePurpose/BasicAddition.java
@@ -0,0 +1,13 @@
+package javaPracticePurpose;
+
+public class BasicAddition 
+{
+	public static void main(String[] args) 
+	{
+		int a,b,c;
+		a=30;
+		b=25;
+		c=a+b;
+		System.out.println("Adition of A+B is -->"+c);
+	}
+}
